5bda03bfcba5ac2777fb4561388ad593586d0274
nuilewis <lewisnuikweh@gmail.com>
2022-06-19 13:07:52 +0100

    init shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flow/Components/flow_bottom_nav_bar.dart';
+import 'package:flow/services/shared_prefs_methods.dart';
+import 'package:flow/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  flowSharedPrefs = await SharedPreferences.getInstance();
+  loadSavedLocations();
+
+  
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.white,
@@ -22,11 +29,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flow',
-      theme: ThemeData(
-        fontFamily: 'Product Sans',
-        scaffoldBackgroundColor: Colors.grey[200],
-        canvasColor: Colors.transparent,
-      ),
+      theme: flowLightTheme(context),
+      darkTheme: flowDarkTheme(context),
+      themeMode: ThemeMode.system,
       home: FlowBottomNavBar(),
       //  home:MyLocation(),
     );
