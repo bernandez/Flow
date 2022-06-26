a3bf67efcdcbfdb584987899bfd15597f6815812
nuilewis <lewisnuikweh@gmail.com>
2022-06-26 14:27:07 +0100

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flow/Components/flow_bottom_nav_bar.dart';
 import 'package:flow/firebase_options.dart';
+import 'package:flow/services/location_service.dart';
 import 'package:flow/services/shared_prefs_methods.dart';
 import 'package:flow/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'providers/flow_water_sources_data_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   flowSharedPrefs = await SharedPreferences.getInstance();
   loadSavedLocations();
-
+  FlowLocationData().getLocation();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.white,
@@ -26,14 +30,20 @@ void main() async {
 class FlowApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flow',
-      theme: flowLightTheme(context),
-      darkTheme: flowDarkTheme(context),
-      themeMode: ThemeMode.system,
-      home: FlowBottomNavBar(),
-      //  home:MyLocation(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<FlowWaterSourcesData>(create: (BuildContext context)=> FlowWaterSourcesData()),
+        ChangeNotifierProvider<FlowLocationData>(create: (BuildContext context)=> FlowLocationData())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title:  'Flow',
+        theme: flowLightTheme(context),
+        darkTheme: flowDarkTheme(context),
+        themeMode: ThemeMode.system,
+        home: FlowBottomNavBar(),
+        //  home:MyLocation(),
+      ),
     );
   }
 }
