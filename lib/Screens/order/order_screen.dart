7cbeecd39aa9c5d0bd45e5bda46e56997065e872
nuilewis <lewisnuikweh@gmail.com>
2022-08-14 17:03:43 +0100

    created order screen

---
+++ lib/Screens/order/order_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+     
+      ),
+    );
+  }
+}
