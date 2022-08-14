77950087d1d2f8144794a4912130bfe45c458825
nuilewis <lewisnuikweh@gmail.com>
2022-08-14 17:03:55 +0100

    created auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class FirebaseAuthService{
+
+  final _auth = FirebaseAuth.instance;
+
+///Register with email and password
+  void registerUser(BuildContext context, {required String email, required String password, String?  firstName, String? lastName}){
+
+    try{
+      final UserCredential? newUser = await _auth.createuserWithEmailAndPassword(email: email,  password: password);
+    
+    if(newUser!=null){
+      Navigator.pushNamed(context, HomeScreen.id);
+    }
+    } catch(e){
+///TODO: show snackbar when failed
+      debugPrint(e);
+    }
+  }
+}
