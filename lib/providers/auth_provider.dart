68a82f337dbc271d276fd28cad74d19583dc0caa
nuilewis <lewisnuikweh@gmail.com>
2022-08-14 20:37:17 +0100

    added user model and auth provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flow/models/flow_user_model.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../services/auth_service.dart';
+
+class FlowUserData extends ChangeNotifier {
+  FlowUser flowUser = FlowUser(
+    firstName: FirebaseAuthService().getCurrentUser()?.displayName,
+    uid: FirebaseAuthService().getCurrentUser()?.uid,
+    email: FirebaseAuthService().getCurrentUser()?.email,
+  );
+}
