23ff35a4365763f6893a685d2082c43848e26eae
bernandez <alouzehm@gmail.com>
2022-09-23 04:01:58 -0500

    removed unused code

--- lib/Screens/order/order_screen.dart
+++ lib/Screens/order/order_screen.dart
@@ -245,7 +245,7 @@ class _FlowOrderScreenState extends State<FlowOrderScreen> {
                               primary: Theme.of(context).primaryColor,
                               shape: new RoundedRectangleBorder(
                       borderRadius: new BorderRadius.circular(37)
-                    )
+                  )
                               
 
                         ),),
@@ -393,96 +393,3 @@ class OrderComfirmationButton extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:flow/constants.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:provider/provider.dart';
-
-// import '../../providers/auth_provider.dart';
-
-// class FlowOrderScreen extends StatelessWidget {
-//   static const id = "order screen";
-//   const FlowOrderScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<FlowUserData>(
-//       builder: (context, userData, child) {
-//         if (userData.flowUser?.uid != null) {
-//           return Scaffold(
-//             body: Column(
-//               children: const [
-//                 Text("You have an account, now you can order"),
-//                 ///TODO: Build ordering page
-//               ],
-//             ),
-//           );
-//         } else {
-//           return Scaffold(
-//               body: Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-//                 child: Center(
-//                   child: Column(
-//                     mainAxisSize: MainAxisSize.min,
-//             children: [
-//                    Text(" You need an account to place and order", style: Theme.of(context).textTheme.bodyText1, textAlign: TextAlign.center,),
-                  
-//                       const SizedBox(height: kDefaultPadding2x,),
-//                   FlowButton(
-//                       iconLink: "",
-//                       onPressed: () {
-//                         ///TODO: nav to sign in page
-//                       },
-//                       iconColor: Colors.white,
-//                       label: "Sign In"),
-
-//                       const SizedBox(height: kDefaultPadding,),
-//                   FlowButton(
-//                     isSecondary: true,
-//                       iconLink: "",
-//                       onPressed: () {
-//                         ///TODO: nav to create account page
-//                       },
-//                       label: "Create Account")
-//             ],
-//           ),
-//                 ),
-//               ));
-//         }
-//       },
-//     );
-//   }
-// }
